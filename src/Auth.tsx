bb59fac20047ea317b7ce766e072c8cf7da4f950
YaninaErriest <yaninaerriest@gmail.com>
2025-06-07 20:03:22 -0300

    modify auth

--- src/Auth.tsx
+++ src/Auth.tsx
@@ -1,17 +1,22 @@
 import { auth, provider } from "./services/firebase"
-import { signInWithRedirect } from "firebase/auth";
-
+import { signInWithPopup } from "firebase/auth";
 
 export const Auth = () => {
   const signInWithGoogle = async () => {
-    await signInWithRedirect(auth, provider);
+    try {
+      const result = await signInWithPopup(auth, provider);
+      console.log("Usuario autenticado:", result.user);
+    } catch (error) {
+      console.error("Error al iniciar sesión:", error);
+    }
   };
 
   return (
-    <button onClick={signInWithGoogle} 
-            className="mt-16 bg-blue-600 hover:bg-blue-700 text-white px-6 py-3 rounded-lg shadow-md hover:shadow-lg transition-all hover:scale-[1.02] active:scale-95 transition-transform "
-            >
-              Login con Google
-              </button>
+    <button
+      onClick={signInWithGoogle}
+      className="mt-16 bg-blue-600 hover:bg-blue-700 text-white px-6 py-3 rounded-lg shadow-md hover:shadow-lg transition-all hover:scale-[1.02] active:scale-95 transition-transform "
+    >
+      Login con Google
+    </button>
   );
 };
